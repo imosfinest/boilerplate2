7c33c8efafdfe6ee3831e054b81d8be0a8f2f6e9
OPARA GOODNESS <68869066+imosfinest@users.noreply.github.com>
2023-01-18 10:46:28 +0400

    Update soon.js

--- js/soon.js
+++ js/soon.js
@@ -2,11 +2,10 @@ const navSlide = () =>{
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
-
     //Button Toggle function
     burger.addEventListener('click', () =>{
         nav.classList.toggle('nav-active');
-
+        nav.classList.toggle('show');
         //Animating nav-links in-out Function
         navLinks.forEach((link, index)=>{
             if(link.style.animation){
@@ -14,15 +13,10 @@ const navSlide = () =>{
             }
             else{
             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-    
             }
-    
         });
 //Burger Animation
         burger.classList.toggle('toggle')
-
     });
-
-    
 }
 navSlide();
